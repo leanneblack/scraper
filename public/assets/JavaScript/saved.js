6067cbc876fc91e948e2135045c5d2ca8c8640d3
leanneblack <leanne.black@cox.net>
2019-05-20 07:54:39 -0700

    initial set up of file structure and server

---
+++ public/assets/JavaScript/saved.js
@@ -0,0 +1,1 @@
+<h1>Scraper App: SAVED</h1>;
---
+++ server.js
@@ -0,0 +1,55 @@
+//requre the dependencies
+let express = require("express");
+let expressHandlebars = require("express-handlebars");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+
+// set up port to the the host or 3000
+let PORT = process.env.PORT || 3000;
+
+// make my express app
+var app = express();
+
+//sets up express router
+let router = express.Router();
+
+// makes public folder the static directory
+app.use(express.static(__dirname + "/public"));
+
+//connect handlebars to express app
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main"
+  })
+);
+
+//use body parser
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// make every request go through the router middleware
+app.use(router);
+
+//database deployed or the local host database mongoHeadlines
+let db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connect mongoose to the database
+mongoose.connect(db, function(error) {
+  //log the errors if there are any
+  if (error) {
+    conosle.log("oh nooo, there is an error");
+
+    // if no errors log a success message
+  } else {
+    console.log("Suhweet mongoose connection successful");
+  }
+});
+
+// listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port:" + PORT + "🌎");
+});
